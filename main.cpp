dde226e2597060c7e8b2108c6666f2844d0c4e67
longyiszh <longyiszh@gmail.com>
2022-12-28 15:44:24 -0500

    running knight

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include "raylib.h"
 #include "raymath.h"
 
+struct AnimationData
+{
+    Rectangle textureBoundary;
+    Vector2 position;
+    int animationframe;
+    float updateTime;
+    float runningTime;
+};
+
 /**
  * Instead of moving character on the map,
  * We fix charcter in the middle of the window
@@ -33,6 +42,42 @@ Vector2 moveMap(Vector2 &mapPosition)
     return direction;
 }
 
+Rectangle *getFrameBoundaries(float frameWidth, float frameHeight, int columnsCount, int rowsCount)
+{
+    Rectangle *frameBoundaries = new Rectangle[columnsCount * rowsCount];
+    for (int xIndex = 0; xIndex < columnsCount; xIndex++)
+    {
+        for (int yIndex = 0; yIndex < rowsCount; yIndex++)
+        {
+            frameBoundaries[yIndex * columnsCount + xIndex] = Rectangle{
+                .x{frameWidth * xIndex},
+                .y{frameHeight * yIndex},
+                .width{frameWidth},
+                .height{frameHeight}};
+        }
+    }
+    return frameBoundaries;
+}
+
+AnimationData updateAnimationData(AnimationData nextAnimData, Rectangle frames[], int animFrameCount, float currentDeltaTime)
+{
+    nextAnimData.runningTime += currentDeltaTime;
+    if (nextAnimData.runningTime >= nextAnimData.updateTime)
+    {
+        nextAnimData.textureBoundary = frames[nextAnimData.animationframe];
+
+        // reset animation stopwatch
+        nextAnimData.runningTime = 0.0;
+        // control frame
+        nextAnimData.animationframe++;
+        if (nextAnimData.animationframe > animFrameCount - 1)
+        {
+            nextAnimData.animationframe = 0;
+        }
+    }
+    return nextAnimData;
+}
+
 int main()
 {
     // Window dimension
@@ -43,13 +88,15 @@ int main()
     Texture2D mapTexture{LoadTexture("map/map0.png")};
     Vector2 mapPosition{}; // Will be initialzed to {0.0f, 0.0f}
 
-    Texture2D knightTexture{LoadTexture("assets/characters/knight_idle_spritesheet.png")};
+    // Textures here must have same width and height
+    Texture2D knightIdleTexture{LoadTexture("assets/characters/knight_idle_spritesheet.png")};
+    Texture2D knightRunningTexture{LoadTexture("assets/characters/knight_run_spritesheet.png")};
 
     Rectangle knightSource{
         .x{0},
         .y{0},
-        .width{knightTexture.width / 6.0f},
-        .height{(float)knightTexture.height}};
+        .width{knightIdleTexture.width / 6.0f},
+        .height{(float)knightIdleTexture.height}};
 
     Vector2 knightPosition{
         // *4: texture is too tiny, so scaling it up
@@ -65,6 +112,24 @@ int main()
     // -1.0f: facing left; 1.0f: facing right
     float knightXDirection{1.0f};
 
+    const int knightFrameColumnsCount{6};
+    const int knightFrameRowsCount{1};
+    const int knightFrameBoundariesCount{knightFrameColumnsCount * knightFrameRowsCount};
+
+    // Rectangle[]
+    Rectangle *knightFrameBoundaries = getFrameBoundaries(
+        knightSource.width,
+        knightSource.height,
+        knightFrameColumnsCount,
+        knightFrameRowsCount);
+
+    AnimationData knightAnimData{
+        .textureBoundary{knightFrameBoundaries[0]},
+        .position{},
+        .animationframe{0},
+        .updateTime{1.0 / 12.0},
+        .runningTime{0}};
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
@@ -74,6 +139,9 @@ int main()
 
         BeginDrawing();
         ClearBackground(WHITE);
+
+        bool knightIdle{false};
+        Texture2D *knightToDrawTexture{};
 
         Vector2 moveDirection{moveMap(mapPosition)};
         if (moveDirection.x <= -1.0f)
@@ -85,7 +153,21 @@ int main()
             knightXDirection = 1.0f;
         }
 
+        if (moveDirection.x == 0.0f && moveDirection.y == 0.0f)
+        {
+            knightIdle = true;
+        }
+
+        // update knight anim
+        knightAnimData = updateAnimationData(
+            knightAnimData,
+            knightFrameBoundaries,
+            knightFrameBoundariesCount,
+            deltaTime);
+
         Rectangle currentKnightSource = knightSource;
+        currentKnightSource.x = knightAnimData.textureBoundary.x;
+        currentKnightSource.y = knightAnimData.textureBoundary.y;
         currentKnightSource.width *= knightXDirection;
 
         // draw map
@@ -96,9 +178,18 @@ int main()
             4.0f,
             WHITE);
 
+        if (knightIdle)
+        {
+            knightToDrawTexture = &knightIdleTexture;
+        }
+        else
+        {
+            knightToDrawTexture = &knightRunningTexture;
+        }
+
         // draw the knight
         DrawTexturePro(
-            knightTexture,
+            *knightToDrawTexture,
             currentKnightSource,
             knightDestination,
             Vector2{0, 0},
@@ -110,7 +201,9 @@ int main()
 
     /// Clean ups
     UnloadTexture(mapTexture);
-    UnloadTexture(knightTexture);
+    UnloadTexture(knightIdleTexture);
+
+    delete[] knightFrameBoundaries;
 
     CloseWindow();
 }
