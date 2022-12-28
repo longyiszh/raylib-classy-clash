f65d7598ae0db1226bbdf58fa5098eb16aa495fc
longyiszh <longyiszh@gmail.com>
2022-12-28 10:06:16 -0500

    add knight

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
  * We fix charcter in the middle of the window
  * And move map
  */
-void moveMap(Vector2 &mapPosition)
+Vector2 moveMap(Vector2 &mapPosition)
 {
     float moveSpeed{8.0f};
     Vector2 direction{};
@@ -30,6 +30,7 @@ void moveMap(Vector2 &mapPosition)
                                           Vector2Normalize(direction),
                                           moveSpeed));
     }
+    return direction;
 }
 
 int main()
@@ -42,14 +43,50 @@ int main()
     Texture2D mapTexture{LoadTexture("map/map0.png")};
     Vector2 mapPosition{}; // Will be initialzed to {0.0f, 0.0f}
 
+    Texture2D knightTexture{LoadTexture("assets/characters/knight_idle_spritesheet.png")};
+
+    Rectangle knightSource{
+        .x{0},
+        .y{0},
+        .width{knightTexture.width / 6.0f},
+        .height{(float)knightTexture.height}};
+
+    Vector2 knightPosition{
+        // *4: texture is too tiny, so scaling it up
+        .x{windowWidth / 2.0f - 4 * (0.5f * knightSource.width)},
+        .y{windowHeight / 2.0f - 4 * (0.5f * knightSource.height)}};
+
+    Rectangle knightDestination{
+        .x{knightPosition.x},
+        .y{knightPosition.y},
+        .width{knightSource.width * 4},
+        .height{knightSource.height * 4}};
+
+    // -1.0f: facing left; 1.0f: facing right
+    float knightXDirection{1.0f};
+
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        // time since last frame
+        float deltaTime{GetFrameTime()};
+
         BeginDrawing();
         ClearBackground(WHITE);
 
-        moveMap(mapPosition);
+        Vector2 moveDirection{moveMap(mapPosition)};
+        if (moveDirection.x <= -1.0f)
+        {
+            knightXDirection = -1.0f;
+        }
+        else if (moveDirection.x >= 1.0f)
+        {
+            knightXDirection = 1.0f;
+        }
+
+        Rectangle currentKnightSource = knightSource;
+        currentKnightSource.width *= knightXDirection;
 
         // draw map
         DrawTextureEx(
@@ -59,11 +96,21 @@ int main()
             4.0f,
             WHITE);
 
+        // draw the knight
+        DrawTexturePro(
+            knightTexture,
+            currentKnightSource,
+            knightDestination,
+            Vector2{0, 0},
+            0.f,
+            WHITE);
+
         EndDrawing();
     }
 
     /// Clean ups
     UnloadTexture(mapTexture);
+    UnloadTexture(knightTexture);
 
     CloseWindow();
 }
